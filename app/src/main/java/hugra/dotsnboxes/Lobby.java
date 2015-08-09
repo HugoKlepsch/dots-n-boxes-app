6143e27d553540f42c57706706da7ea31134286c
Hugo Klepsch <hugo.klepsch@gmail.com>
2015-08-09 15:09:21 -0400

    Fixed broken ID

--- app/src/main/java/hugra/dotsnboxes/Lobby.java
+++ app/src/main/java/hugra/dotsnboxes/Lobby.java
@@ -28,7 +28,7 @@ public class Lobby extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_LogOut) {
             return true;
         }
 
