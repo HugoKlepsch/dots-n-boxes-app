f1833a52ee89d0e26151153fbea0f24ecf35fa4b
Graham <ghildebrandt@outlook.com>
2015-08-09 15:28:05 -0400

    more in and out comms

--- app/src/main/java/hugra/dotsnboxes/InComms.java
+++ app/src/main/java/hugra/dotsnboxes/InComms.java
@@ -1,15 +1,42 @@
 package hugra.dotsnboxes;
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.net.InetAddress;
+import java.net.Socket;
+
+import sharedPackages.ActionRequest;
+
 /**
  * Created by graham on 09/08/15.
  */
 public class InComms extends Thread {
 
-    public InComms(){
+    private Socket scSock;
+    private ObjectInputStream scStream;
+
+    public InComms(Socket socket) throws IOException{
+        scSock = socket;
+
 
     }
 
     public void run(){
+        try {
+            scStream = new ObjectInputStream(scSock.getInputStream());
+            ActionRequest actionRequest;
+            while(Lobby.stayAlive){
+                actionRequest = (ActionRequest)scStream.readObject();
+
+            }
+
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
 
     }
 }
--- app/src/main/java/hugra/dotsnboxes/OutComms.java
+++ app/src/main/java/hugra/dotsnboxes/OutComms.java
@@ -1,7 +1,35 @@
 package hugra.dotsnboxes;
 
+import android.app.Notification;
+
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
+import sharedPackages.ActionRequest;
+import sharedPackages.User;
+
 /**
  * Created by graham on 09/08/15.
  */
 public class OutComms extends Thread {
+    private Socket csSock;
+    private ObjectOutputStream csStream;
+
+    public OutComms(Socket socket){
+        csSock = socket;
+    }
+
+    public void run(){
+        try {
+            csStream = new ObjectOutputStream(csSock.getOutputStream());
+//            csStream.writeObject(new ActionRequest(ActionRequest.CS_CONNECT, new User()));
+            while(Lobby.stayAlive){
+                csStream.writeObject(new ActionRequest(ActionRequest.CS_USERLIST));
+                csStream.flush();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
