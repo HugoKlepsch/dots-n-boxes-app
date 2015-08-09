18566637caf8a89b34994029f8111c2d19013e9d
Graham <ghildebrandt@outlook.com>
2015-08-09 15:04:50 -0400

    Started in and out comms

---
+++ app/src/main/java/hugra/dotsnboxes/InComms.java
@@ -0,0 +1,15 @@
+package hugra.dotsnboxes;
+
+/**
+ * Created by graham on 09/08/15.
+ */
+public class InComms extends Thread {
+
+    public InComms(){
+
+    }
+
+    public void run(){
+
+    }
+}
---
+++ app/src/main/java/hugra/dotsnboxes/OutComms.java
@@ -0,0 +1,7 @@
+package hugra.dotsnboxes;
+
+/**
+ * Created by graham on 09/08/15.
+ */
+public class OutComms extends Thread {
+}
