8995923b2af6265835ff5cf38dc73048c21dc604
Hugo Klepsch <hugo.klepsch@gmail.com>
2015-08-09 15:17:16 -0400

    Stayalive is now static

--- app/src/main/java/hugra/dotsnboxes/Lobby.java
+++ app/src/main/java/hugra/dotsnboxes/Lobby.java
@@ -6,7 +6,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 public class Lobby extends AppCompatActivity {
-    public boolean stayAlive = true;
+    public static boolean stayAlive = true;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
